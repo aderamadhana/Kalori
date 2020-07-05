0519c27a423c0eda6cbe1920c9c45f1a8ec7a25f
Ade Ramadhana Pratama <171111020@mhs.stiki.ac.id>
2020-07-05 19:17:49 +0700

    mari

--- app/src/main/java/com/example/kalori/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/kalori/ui/home/HomeFragment.java
@@ -3,6 +3,7 @@ package com.example.kalori.ui.home;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -35,6 +37,9 @@ import com.example.kalori.AppPreference;
 import com.example.kalori.R;
 import com.example.kalori.SettingActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
@@ -47,7 +52,6 @@ public class HomeFragment extends Fragment {
     private Integer stepCount = 0;
     private TextView textViewGoals, txtJarak, txtSteps, txtBakar, txtSetting;
     private String usia, tinggi, berat, jk;
-    private double lat_awal=0, long_awal=0, lat_akhir=0, long_akhir=0;
     private double hitung;
     private Button stop, start;
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -95,41 +99,18 @@ public class HomeFragment extends Fragment {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(getActivity(),
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE_LOCATION_PERMISSION);
-                }else{
-                    FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(v.getContext());
-                    mFusedLocation.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            if (location != null){
-
-                                Toast.makeText(getActivity(), String.valueOf(location.getLatitude()), Toast.LENGTH_SHORT).show();
-                                Toast.makeText(getActivity(), String.valueOf(location.getLongitude()), Toast.LENGTH_LONG).show();
-
-                                AppPreference.removeLatAwal(getActivity());
-                                AppPreference.removeLongAwal(getActivity());
-                                AppPreference.removeLatAkhir(getActivity());
-                                AppPreference.removeLongAkhir(getActivity());
-
-                                AppPreference.saveLatAwal(getActivity(), String.valueOf(location.getLatitude()));
-                                AppPreference.saveLongAwal(getActivity(), String.valueOf(location.getLongitude()));
-
-                                Log.e("lat", AppPreference.getLatAwal(getActivity()));
-                                Log.e("long", AppPreference.getLongAwal(getActivity()));
-
-                                start.setVisibility(View.GONE);
-                                stop.setVisibility(View.VISIBLE);
-
-                                //Toast.makeText(MainActivity.this, String.valueOf(distance(Double.parseDouble(AppPreference.getLatAwal(MainActivity.this)), Double.parseDouble(AppPreference.getLongAwal(MainActivity.this)), -7.939878, 112.637960)), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-
+                if (ContextCompat.checkSelfPermission(
+                        getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            getActivity(),
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_LOCATION_PERMISSION);
+                } else {
+                    start.setVisibility(View.GONE);
+                    stop.setVisibility(View.VISIBLE);
+                    getFirstLocation();
                 }
-
                 SensorManager sensorManager = (SensorManager) getContext().getSystemService(SENSOR_SERVICE);
                 Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
                 SensorEventListener stepDetector = new SensorEventListener() {
@@ -148,7 +129,6 @@ public class HomeFragment extends Fragment {
                                 stepCount++;
                             }
                             txtSteps.setText(stepCount.toString());
-
                         }
                     }
 
@@ -172,42 +152,141 @@ public class HomeFragment extends Fragment {
                 
                 start.setVisibility(View.VISIBLE);
                 stop.setVisibility(View.GONE);
-                FusedLocationProviderClient mFusedLocation = LocationServices.getFusedLocationProviderClient(v.getContext());
-                mFusedLocation.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
-                    @RequiresApi(api = Build.VERSION_CODES.M)
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null){
-                            Toast.makeText(getActivity(), String.valueOf(location.getLatitude()), Toast.LENGTH_SHORT).show();
-                            Toast.makeText(getActivity(), String.valueOf(location.getLongitude()), Toast.LENGTH_LONG).show();
 
-                            AppPreference.saveLatAkhir(getActivity(), String.valueOf(location.getLatitude()));
-                            AppPreference.saveLongAkhir(getActivity(), String.valueOf(location.getLongitude()));
-
-                            start.setVisibility(View.VISIBLE);
-                            stop.setVisibility(View.GONE);
-
-                            lat_akhir = Double.parseDouble(String.valueOf(AppPreference.getLatAkhir(getActivity())));
-                            long_akhir = Double.parseDouble(String.valueOf(AppPreference.getLongAkhir(getActivity())));
-                            lat_awal = Double.parseDouble(String.valueOf(AppPreference.getLatAwal(getActivity())));
-                            long_awal = Double.parseDouble(String.valueOf(AppPreference.getLongAwal(getActivity())));
-
-                            Log.e("lat1", String.valueOf(lat_awal));
-                            Log.e("long1",String.valueOf(long_awal));
-                            Log.e("lat2", String.valueOf(lat_akhir));
-                            Log.e("long2", String.valueOf(long_akhir));
-
-                            double jarak = distance(lat_awal, long_awal, lat_akhir, long_akhir);
-                            txtJarak.setText(String.format("%.2f", jarak) + " m");
-                                                        //Toast.makeText(MainActivity.this, String.valueOf(distance(Double.parseDouble(AppPreference.getLatAwal(MainActivity.this)), Double.parseDouble(AppPreference.getLongAwal(MainActivity.this)), Double.parseDouble(AppPreference.getLatAkhir(MainActivity.this)), Double.parseDouble(AppPreference.getLongAkhir(MainActivity.this)))), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
+                if (ContextCompat.checkSelfPermission(
+                        getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(
+                            getActivity(),
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_CODE_LOCATION_PERMISSION);
+                } else {
+                    start.setVisibility(View.VISIBLE);
+                    stop.setVisibility(View.GONE);
+                    getLastLocation();
+                }
             }
         });
 
 
         return root;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(start.isPressed()){
+                    getFirstLocation();
+                }else if(stop.isPressed()){
+                    getLastLocation();
+                }
+            } else {
+                Toast.makeText(getContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    public void getFirstLocation() {
+        final ProgressDialog dialog = ProgressDialog.show(
+                getContext(), "Sedang mendapatkan lokasi", "Silahkan tunggu...", true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    LocationRequest locationRequest = new LocationRequest();
+                    locationRequest.setInterval(10000);
+                    locationRequest.setFastestInterval(3000);
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(
+                                getActivity(),
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                REQUEST_CODE_LOCATION_PERMISSION);
+                    } else {
+                        LocationServices.getFusedLocationProviderClient(getContext())
+                                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                    @Override
+                                    public void onLocationResult(LocationResult locationResult) {
+                                        super.onLocationResult(locationResult);
+                                        LocationServices.getFusedLocationProviderClient(getContext())
+                                                .removeLocationUpdates(this);
+                                        if (locationResult != null && locationResult.getLocations().size() > 0) {
+                                            int latestLocationIndex = locationResult.getLocations().size() - 1;
+                                            double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+                                            AppPreference.saveLatAwal(getContext(), String.valueOf(latitude));
+                                            AppPreference.saveLongAwal(getContext(), String.valueOf(longitude));
+
+                                        }
+                                    }
+                                }, Looper.getMainLooper());
+                    }
+                    Thread.sleep(5000);
+                    /*hentikan dialog*/
+                    dialog.dismiss();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    public void getLastLocation() {
+        final ProgressDialog dialog = ProgressDialog.show(
+                getContext(), "Sedang kalkulasi data", "Silahkan tunggu...", true);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    LocationRequest locationRequest = new LocationRequest();
+                    locationRequest.setInterval(10000);
+                    locationRequest.setFastestInterval(3000);
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+                    if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(
+                                getActivity(),
+                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                REQUEST_CODE_LOCATION_PERMISSION);
+                    } else {
+                        LocationServices.getFusedLocationProviderClient(getContext())
+                                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                    @Override
+                                    public void onLocationResult(LocationResult locationResult) {
+                                        super.onLocationResult(locationResult);
+                                        LocationServices.getFusedLocationProviderClient(getContext())
+                                                .removeLocationUpdates(this);
+                                        if (locationResult != null && locationResult.getLocations().size() > 0) {
+                                            int latestLocationIndex = locationResult.getLocations().size() - 1;
+                                            double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
+                                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
+
+                                            AppPreference.saveLatAkhir(getContext(), String.valueOf(latitude));
+                                            AppPreference.saveLongAkhir(getContext(), String.valueOf(longitude));
+
+                                            double getLatAwal = Double.parseDouble(AppPreference.getLatAwal(getContext()));
+                                            double getLongAwal = Double.parseDouble(AppPreference.getLongAwal(getContext()));
+                                            double getLatAkhir = Double.parseDouble(AppPreference.getLatAkhir(getContext()));
+                                            double getLongAkhir = Double.parseDouble(AppPreference.getLongAkhir(getContext()));
+
+                                            double distance = distance(getLatAwal, getLongAwal, getLatAkhir, getLongAkhir);
+                                            txtJarak.setText(String.format("%.2f", distance)+" m");
+                                        }
+                                    }
+                                }, Looper.getMainLooper());
+                    }
+
+                    Thread.sleep(5000);
+                    dialog.dismiss();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private double distance(double lat1, double lon1, double lat2, double lon2) {
@@ -219,7 +298,7 @@ public class HomeFragment extends Fragment {
                 * Math.cos(deg2rad(theta));
         dist = Math.acos(dist);
         dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
+        dist = (dist * 60 * 1.1515)*1000;
         return (dist);
     }
 
